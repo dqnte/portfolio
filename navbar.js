1abc0f8d71d33c62c14690d2824dfcc8feb68fc4
Dante Tobar <dante.m.tobar@gmail.com>
2020-04-03 15:14:26 -0700

    navbar redesign and some mobile style tweeks

--- navbar.js
+++ navbar.js
@@ -1,10 +1,18 @@
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  const links = document.querySelector('.project-links');
+  if (prevScrollpos - currentScrollPos >= 0) {
     document.querySelector('.header').style.top = '0';
+    if (links) {
+      links.style.top = '0';
+    }
   } else {
     document.querySelector('.header').style.top = '-20vh';
+    if (links) {
+      links.style.top = '-20vh';
+    }
   }
+
   prevScrollpos = currentScrollPos;
 };
